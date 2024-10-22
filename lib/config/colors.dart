9e0c14d29320005a7c232384e0a9ba27dc5e15a3
kasun Hasanga <Kasunhasanga96@gmail.com>
2024-10-22 12:10:11 +0530

    color theme improve

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -6,49 +6,22 @@ extension AppColors on BuildContext {
   static const Color kThemeBackgroundLight = Color(0xFFFFFFFF);
 
   static const Color kGreen = Color(0xFF62C177);
-  static const Color kLightGreen = Color(0xFF4CAF50);
-  static const Color kSeaGreen = Color(0xFF62C17B);
-  static const Color kPastelMintGreen = Color(0xFFC2E7C9);
-  static const Color kTextGreen = Color(0xFF76D13E);
-  static const Color kCardBgGreen = Color(0xFFEBFFEF);
-  static const Color kCardBorderGray = Color(0xFFF3CBB1);
-  static const Color kCardBgGray = Color(0xFFFFF2EA);
+
+  static const Color kOnPrimary = Color(0xFF4CAF50);
+  static const Color kSecondary =Color(0xFFF0FDF8);
+  static const Color kOnSecondary =  Color(0xFF4CAF50);
+  static const Color kShadowColor = Color(0xFFD6DDEB);
   static const Color kGreenBorder = Color(0xFF97D5D5);
-  static const Color kGridGreen = Color(0xFFDFEFE2);
-  static const Color kLighterGreen = Color(0xFFF0FDF8);
+
   static const Color kTransparent = Color(0x00FFFFFF);
   static const Color kWhite = Color(0xFFFFFFFF);
-  static const Color kWhite60 = Color(0x60FFFFFF);
   static const Color kStroke26 = Color(0x268F92A1);
   static const Color kBlack20 = Color(0x20000000);
   static const Color kBlack = Color(0xFF000000);
-  static const Color kTextGray = Color(0xFF575A66);
-  static const Color kLightGray = Color(0xFFE0E6EB);
   static const Color kTextLightGray = Color(0xFF8A8C95);
-  static const Color kTextMediumGray = Color(0xFF9FA2B0);
-  static const Color kTextBlack = Color(0xFF202330);
-  static const Color kTextDarkGray = Color(0xFF888B97);
   static const Color kBorderColor = Color(0xFFE0E0E7);
   static const Color kRed = Color(0xFFFF6D51);
-  static const Color kLightOrange = Color(0xFFf69844);
-  static const Color kLightBlue = Color(0xFF78C3E3);
-  static const Color kLighterBlue = Color(0xFFBCEFFF);
-  static const Color kTextDarkBlue = Color(0xFF0B3D82);
-  static const Color kTextBlue = Color(0xFF235EB0);
-  static const Color kBlueBg = Color(0xFF1D349A);
-  static const Color kShadeBlue = Color(0xFFE8EFFF);
-  static const Color kBorderBlue = Color(0xFFCADEF0);
-  static const Color kBlack60 = Color(0x603C3C43);
-  static const Color kLoginDivider = Color(0xFFD6DDEB);
-
-
-  static const Color kShadow = Color(0x259BA0B7);
   static const Color kBgColor = Color(0xFFEFEFF2);
 
-
-
-
-
-  //linear Gradient
 
 }
